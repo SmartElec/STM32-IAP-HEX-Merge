c41b50f0645df4d5bf9a6782fdae8ce134253aba
SmartElec <646208823@qq.com>
2018-07-20 18:10:31 +0800

    new

---
+++ Hex文件合并/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Hex文件合并
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btn_merge = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textOutPath = new System.Windows.Forms.TextBox();
+            this.btn_outpath = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_help = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(6, 20);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(269, 29);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(6, 65);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(269, 28);
+            this.textBox2.TabIndex = 1;
+            // 
+            // btn_merge
+            // 
+            this.btn_merge.Location = new System.Drawing.Point(297, 217);
+            this.btn_merge.Name = "btn_merge";
+            this.btn_merge.Size = new System.Drawing.Size(66, 35);
+            this.btn_merge.TabIndex = 2;
+            this.btn_merge.Text = "合并";
+            this.btn_merge.UseVisualStyleBackColor = true;
+            this.btn_merge.Click += new System.EventHandler(this.btn_merge_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(281, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(66, 73);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "选择文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textOutPath
+            // 
+            this.textOutPath.Location = new System.Drawing.Point(6, 20);
+            this.textOutPath.Multiline = true;
+            this.textOutPath.Name = "textOutPath";
+            this.textOutPath.Size = new System.Drawing.Size(269, 30);
+            this.textOutPath.TabIndex = 4;
+            // 
+            // btn_outpath
+            // 
+            this.btn_outpath.Location = new System.Drawing.Point(285, 20);
+            this.btn_outpath.Name = "btn_outpath";
+            this.btn_outpath.Size = new System.Drawing.Size(66, 30);
+            this.btn_outpath.TabIndex = 5;
+            this.btn_outpath.Text = "默认目录";
+            this.btn_outpath.UseVisualStyleBackColor = true;
+            this.btn_outpath.Click += new System.EventHandler(this.btn_outpath_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(18, 227);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(120, 16);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "合并时写入特殊值";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(357, 107);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "需要合并的hex文件 for stm32";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textOutPath);
+            this.groupBox2.Controls.Add(this.btn_outpath);
+            this.groupBox2.Location = new System.Drawing.Point(12, 134);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(358, 75);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "输出目录选择";
+            // 
+            // btn_help
+            // 
+            this.btn_help.Location = new System.Drawing.Point(190, 222);
+            this.btn_help.Name = "btn_help";
+            this.btn_help.Size = new System.Drawing.Size(62, 29);
+            this.btn_help.TabIndex = 9;
+            this.btn_help.Text = "帮助";
+            this.btn_help.UseVisualStyleBackColor = true;
+            this.btn_help.Click += new System.EventHandler(this.btn_help_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 260);
+            this.Controls.Add(this.btn_help);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btn_merge);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HEX合并 for stm32 IAP";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btn_merge;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textOutPath;
+        private System.Windows.Forms.Button btn_outpath;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_help;
+    }
+}
+
---
+++ Hex文件合并/Form1.cs
@@ -0,0 +1,420 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Hex文件合并
+{
+    public partial class Form1 : Form
+    {
+        //hex格式解析：<0x3a>[数据长度1Byte][数据地址2Byte][数据类型1Byte][数据nByte][校验1Byte]<0x0d><0x0a>
+        /*
+        '00' Data Record 数据
+        '01' End of File Record 文件结束标志
+        '02' Extended Segment Address Record 延伸段地址
+        '03' Start Segment Address Record   起始延伸地址
+        '04' Extended Linear Address Record 扩展线性地址 也就是基地址
+        '05' Start Linear Address Record       程序起始地址也就是程序入口地址(main)
+        0800 这个就是基地址(0x0800<<16)
+         */
+        struct DataLineMessage
+        {
+            public byte length;
+            public UInt32 addr;
+            public byte type;
+            public UInt32 ExtAddr;//数据域
+            public byte checksum;
+        };  
+        public Form1()
+        {
+            InitializeComponent();
+            checkBox1.Checked = false;
+            btn_help.Visible = false;
+        }
+        bool ParaseFileLine(string inoutstr,out DataLineMessage formatnew)
+        {
+            formatnew.length = 0;
+            formatnew.type = 0;
+            formatnew.addr = 0;
+            formatnew.ExtAddr = 0;
+            formatnew.checksum = 0;
+            try
+            {
+                //DataLineMessage line=new DataLineMessage();
+                byte[] data = HexStringToByteArray(inoutstr.Substring(1));
+                if ((inoutstr.Substring(0, 1) != ":"))
+                {
+                    return false;
+                }
+                if(data.Length != 1+2+1+1+ data[0])
+                {
+                    return false;
+                }
+                //长度
+                formatnew.length = data[0];
+                //数据地址
+                formatnew.addr = (UInt32)((data[1] << 8) | (data[2] << 0));
+                //数据类型
+                formatnew.type = data[3];
+
+                if((formatnew.type<=0x05)&&(formatnew.type>=0x02))
+                {
+                    //扩展地址
+                    if (formatnew.length==2)
+                    {
+                        formatnew.ExtAddr = (UInt32)((data[4] << 8) | (data[5] << 0));
+                        formatnew.ExtAddr <<= 16;
+                    }
+                    else if(formatnew.length==4)
+                    {
+                        formatnew.ExtAddr = (UInt32)((data[4] << 8) | (data[5] << 0));
+                        formatnew.ExtAddr |= (UInt32)((data[6] << 8) | (data[7] << 0));
+                    }
+                }
+                formatnew.checksum =data[data.Length - 1];
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open_fd = new OpenFileDialog();
+            open_fd.Multiselect = true;
+            open_fd.Title = @"请选择文件";
+            open_fd.Filter = @"所有文件(*.*)|*.*|HEX文件(*.hex)|*.hex";
+            open_fd.FilterIndex = 2;
+            if (open_fd.ShowDialog() == DialogResult.OK)
+            {
+                //Multiselect=true后  open_fd.FileNames是一个字符串数组
+                FileStream filefd = null;
+                StreamReader streamReader = null;
+
+                if(open_fd.FileNames.Length<=2)
+                {
+                    DataLineMessage line = new DataLineMessage();
+                    filefd = new FileStream(open_fd.FileNames[0], FileMode.Open, FileAccess.Read);
+                    streamReader = new StreamReader(filefd, Encoding.Default);
+                    filefd.Seek(0, SeekOrigin.Begin);
+                    string content = streamReader.ReadLine();
+                    if(ParaseFileLine(content,out line))
+                    {
+                        UInt32 BaseAddr = line.ExtAddr;
+                        Debug.WriteLine("line.ExtAddr=0x{0:x8}", BaseAddr);//基地址                 
+                        content = streamReader.ReadLine();
+                        if(ParaseFileLine(content,out line))
+                        {
+                            BaseAddr |= line.addr;//加上偏移地址
+                            if (BaseAddr == 0x08000000)
+                            {
+                                //IsBootLoader = true;
+                                textBox1.Text = open_fd.FileNames[0];
+                                if (open_fd.FileNames.Length == 2)
+                                    textBox2.Text = open_fd.FileNames[1];
+                            }
+                            else
+                            {
+                                textBox2.Text = open_fd.FileNames[0];
+                                if (open_fd.FileNames.Length == 2)
+                                    textBox1.Text = open_fd.FileNames[1];
+                            }
+                            textOutPath.Text = GetNewPathForDupes(System.IO.Path.GetDirectoryName(open_fd.FileNames[0]) + @"\merge.hex");
+                        }
+                        else
+                        {
+                            textBox1.Text = "";
+                            textBox2.Text = "";
+                            MessageBox.Show("file is not correct", "error");
+                        }
+                    }
+                    else
+                    {
+                        textBox1.Text = "";
+                        textBox2.Text = "";
+                        MessageBox.Show("file is not correct", "error");
+                    }
+                    if (filefd != null)
+                    {
+                        filefd.Close();
+                    }
+                    if (streamReader != null)
+                    {
+                        streamReader.Close();
+                    } 
+                }
+                else
+                {
+                    MessageBox.Show("there are too many files","error");
+                }
+                
+            }
+        }
+        private void btn_merge_Click(object sender, EventArgs e)
+        {
+            string str1, str2;
+            str1 = textBox1.Text.Trim();
+            str2 = textBox2.Text.Trim();
+            if (str1 == str2)
+            {
+                MessageBox.Show("please select diffirent file", "error");
+                return;
+            }
+            if(str1=="")
+            {
+                MessageBox.Show("please select bootloader file", "error");
+                return;
+            }
+            if (str2 == "")
+            {
+                MessageBox.Show("please select app file", "error");
+                return;
+            }
+            Debug.WriteLine(str1);
+            Debug.WriteLine(str2);
+            StreamReader fileReader=null;
+            StreamWriter Newfile = null;
+            
+            try
+            {
+                DataLineMessage line = new DataLineMessage();
+                textOutPath.Text = GetNewPathForDupes(textOutPath.Text);
+
+                //读取文件一
+                fileReader = new StreamReader(str1);
+                Newfile = new StreamWriter(textOutPath.Text);
+                string strline = null;
+                UInt32 Nowaddr=0;
+                do
+                {
+                    strline = fileReader.ReadLine();
+                    if (ParaseFileLine(strline, out line))
+                    {
+
+                        if(line.type==0x01)//结束标志
+                        {
+                            break;
+                        }
+                        else if (line.type == 0x05)//入口地址
+                        {
+                            if(checkBox1.Checked)
+                            {
+                                if (Nowaddr <= 0x2ff8)
+                                {
+                                    Newfile.WriteLine(":022FFA00A5A58B");//往地址（基地址+0x2FFA）写入0xA5A5
+                                }
+                                else
+                                {
+                                    checkBox1.Checked = false;
+                                    MessageBox.Show("bootloader过大，不支持特殊字节写入");
+                                }
+                                
+                            }
+                            Newfile.WriteLine(strline);
+                        }
+                        else//数据等
+                        {
+                            Nowaddr = line.addr;
+                            Newfile.WriteLine(strline);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("There have some error in file","merge faild");
+                        return;
+                    }
+
+                } while (strline != null);
+                //读取文件二 Newfile.WriteLine
+                fileReader = new StreamReader(str2);
+                do
+                {
+                    strline = fileReader.ReadLine();
+                    Newfile.WriteLine(strline);
+                } while (strline != null);
+                MessageBox.Show("merge successful");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (fileReader != null)
+                {
+                    fileReader.Close();
+                }
+                if (Newfile != null)
+                {
+                    Newfile.Close();
+                } 
+            }
+        }
+
+        #region 字符串转换函数
+        //翻转byte数组
+        public static void ReverseBytes(byte[] bytes)
+        {
+            byte tmp;
+            int len = bytes.Length;
+
+            for (int i = 0; i < len / 2; i++)
+            {
+                tmp = bytes[len - 1 - i];
+                bytes[len - 1 - i] = bytes[i];
+                bytes[i] = tmp;
+            }
+        }
+        //规定转换起始位置和长度
+        public static void ReverseBytes(byte[] bytes, int start, int len)
+        {
+            int end = start + len - 1;
+            byte tmp;
+            int i = 0;
+            for (int index = start; index < start + len / 2; index++, i++)
+            {
+                tmp = bytes[end - i];
+                bytes[end - i] = bytes[index];
+                bytes[index] = tmp;
+            }
+        }
+
+        // 翻转字节顺序 (16-bit)
+        public static UInt16 ReverseBytes(UInt16 value)
+        {
+            return (UInt16)((value & 0xFFU) << 8 | (value & 0xFF00U) >> 8);
+        }
+
+
+        // 翻转字节顺序 (32-bit)
+        public static UInt32 ReverseBytes(UInt32 value)
+        {
+            return (value & 0x000000FFU) << 24 | (value & 0x0000FF00U) << 8 |
+                   (value & 0x00FF0000U) >> 8 | (value & 0xFF000000U) >> 24;
+        }
+        // 翻转字节顺序 (64-bit)
+        public static UInt64 ReverseBytes(UInt64 value)
+        {
+            return (value & 0x00000000000000FFUL) << 56 | (value & 0x000000000000FF00UL) << 40 |
+                   (value & 0x0000000000FF0000UL) << 24 | (value & 0x00000000FF000000UL) << 8 |
+                   (value & 0x000000FF00000000UL) >> 8 | (value & 0x0000FF0000000000UL) >> 24 |
+                   (value & 0x00FF000000000000UL) >> 40 | (value & 0xFF00000000000000UL) >> 56;
+        }
+        public string ByteArrayToHexString(byte[] data)
+        {
+            StringBuilder sb = new StringBuilder(data.Length * 3);
+            foreach (byte b in data)
+                sb.Append(Convert.ToString(b, 16).PadLeft(2, '0'));
+            return sb.ToString().ToUpper();
+        }
+
+        public byte[] HexStringToByteArray(string s)
+        {
+            s = s.Replace(" ", "");
+            if (s.Length % 2 != 0)
+            {
+                s = s.Substring(0, s.Length - 1) + "0" + s.Substring(s.Length - 1);
+            }
+            byte[] buffer = new byte[s.Length / 2];
+
+            try
+            {
+                for (int i = 0; i < s.Length; i += 2)
+                    buffer[i / 2] = (byte)Convert.ToByte(s.Substring(i, 2), 16);
+                return buffer;
+            }
+            catch
+            {
+                string errorString = "E4";
+                byte[] errorData = new byte[errorString.Length / 2];
+                errorData[0] = (byte)Convert.ToByte(errorString, 16);
+                return errorData;
+            }
+        }
+
+        public string StringToHexString(string s)
+        {
+            s = s.Replace(" ", "");
+            string buffer = "";
+            char[] myChar;
+            myChar = s.ToCharArray();
+            for (int i = 0; i < s.Length; i++)
+            {
+                buffer = buffer + Convert.ToString(myChar[i], 16);
+                buffer = buffer.ToUpper();
+            }
+            return buffer;
+        }
+        #endregion
+        /// <summary>
+        /// Generates a new path for duplicate filenames.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <returns></returns>
+        private string GetNewPathForDupes(string path)
+        {
+            string newFullPath = path.Trim();
+            //if (System.IO.File.Exists(path))
+            //    MessageBox.Show("存在");
+            //else
+            //    MessageBox.Show("不存在");
+            if (System.IO.File.Exists(path))
+            {
+                string directory = Path.GetDirectoryName(path);
+                string filename = Path.GetFileNameWithoutExtension(path);
+                string extension = Path.GetExtension(path);
+                int counter = 1;
+                do
+                {
+                    string newFilename = string.Format("{0}({1}){2}", filename, counter, extension);
+                    newFullPath = Path.Combine(directory, newFilename);
+                    counter++;
+                } while (System.IO.File.Exists(newFullPath));
+            }
+            return newFullPath;
+        }
+        private void btn_outpath_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveImageDialog = new SaveFileDialog();
+            saveImageDialog.FileName = "merge";
+            saveImageDialog.Title = "保存";
+            saveImageDialog.Filter = @"HEX文件|*.hex";
+            if (saveImageDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = saveImageDialog.FileName.ToString();
+                textOutPath.Text = fileName;
+            }
+        }
+
+        private void btn_help_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("在起始地址偏移0X2FFA的位置写入字节0xA5A5\r\n因此需要使用此功能时Bootloader大小不应超过0x2FFA Byte\r\n", "写入特殊值说明");
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            //Debug.WriteLine("checkBox1_CheckedChanged");
+            if(checkBox1.Checked==true)
+            {
+                btn_help.Visible=true;
+            }
+            else
+            {
+                btn_help.Visible = false;
+            }
+            
+        }
+
+    }
+}
