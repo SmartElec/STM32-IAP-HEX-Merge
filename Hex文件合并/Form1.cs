b0f7e17fae76a573a52588af4daac73f80a2abc1
SmartElec <646208823@qq.com>
2022-05-07 13:11:00 +0800

    fix ExtAddr

--- Hex文件合并/Form1.cs
+++ Hex文件合并/Form1.cs
@@ -71,14 +71,10 @@ namespace Hex文件合并
                 if((formatnew.type<=0x05)&&(formatnew.type>=0x02))
                 {
                     //扩展地址
-                    if (formatnew.length==2)
+		    formatnew.ExtAddr = (UInt32)((data[4] << 8) | (data[5] << 0));
+		    formatnew.ExtAddr <<= 16;
+                    if(formatnew.length==4)
                     {
-                        formatnew.ExtAddr = (UInt32)((data[4] << 8) | (data[5] << 0));
-                        formatnew.ExtAddr <<= 16;
-                    }
-                    else if(formatnew.length==4)
-                    {
-                        formatnew.ExtAddr = (UInt32)((data[4] << 8) | (data[5] << 0));
                         formatnew.ExtAddr |= (UInt32)((data[6] << 8) | (data[7] << 0));
                     }
                 }
